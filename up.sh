9ed93d53cf3ca96683128cefe80e995d3d9acc7c
Jonathan Schleucher <jonathan.schleucher@acidresden.de>
2024-12-11 15:34:50 +0100

    add ripgrep

--- up.sh
+++ up.sh
@@ -21,6 +21,9 @@ tmux source ~/.tmux.conf
 # rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# ripgrep for <SPACE>-fg in nvim
+sudo apt install ripgrep
+
 # echo some commands that have to be done manually (install tmux plugins)
 echo '(1) You now need to install tmux plugins with CTRL-Space, I'
 
